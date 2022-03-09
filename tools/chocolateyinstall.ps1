4a76753a430f95d3ed22d37736af8f5566e9b7a4
Cthulhux <>
2022-03-09 16:51:32 +0000

    1.66
    FossilOrigin-Name: d13d7b8cbca11f81db2e0ae644018284a61b1608608145361146a137a268a59e

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName        = 'Mpxplay'
+$version            = '1.0.0'
+$url                = "https://sourceforge.net/projects/mpxplay/files/Mpxplay/Mpxplay v1.66/mpxp166w.zip/download?use_mirror=autoselect"
+$checksum           = "C8D336EBF6EE8169A1F1908E7E4D8FE0B92D6228F4166582B9DCBED35F0BBAAB"
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  unzipLocation = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksumType  = 'sha256'
+  checksum      = $checksum
+}
+
+Install-ChocolateyZipPackage @packageArgs
